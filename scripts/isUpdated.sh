001df2dafc721048b998c03efb9bf9f5fefaa70b
Afonso Costa <afonsoc1997@gmail.com>
2022-05-17 23:37:08 +0100

    Corrected release is the same

--- scripts/isUpdated.sh
+++ scripts/isUpdated.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 ext_tag=$(curl -s \
             -H "Accept: application/vnd.github.v3+json" \
             https://api.github.com/repos/cloudflare/cloudflared/releases?page=1 | jq -r '.[0]["tag_name"]')
@@ -10,7 +8,7 @@ echo "**** External release is $ext_tag"
 
 last_tag=$(curl -s \
             -H "Accept: application/json" \
-            https://hub.docker.com/v2/repositories/afonsoc12/docker-cloudflared/tags | jq -r '.["results"][1]["name"]')
+            https://hub.docker.com/v2/repositories/afonsoc12/cloudflared/tags | jq -r '.["results"][1]["name"]')
 
 echo "**** Last release is $ext_tag"
 
@@ -20,7 +18,7 @@ if [ -z "${ext_tag}" ] || [ "${ext_tag}" == "null" ]; then
 elif [ -z "${last_tag}" ]; then
     echo "**** Can't retrieve last release, exiting... ****"
     exit 1
-elif [ "$ext_tag" == "$last_tag"]; then
+elif [ "$ext_tag" == "$last_tag" ]; then
     echo "**** Current release is still up-to-date ****"
     echo "    - Cloudflared version:  $ext_tag"
     echo "    - Last release version: $last_tag"
